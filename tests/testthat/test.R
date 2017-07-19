c06e55a321143147209cd96612051b13394f822c
nykamp <m.nykamp@fu-berlin.de>
2017-07-19 11:52:21 +0200

    updated

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -2,4 +2,6 @@ library(testpkg)
 context("basic test")
 test_that("my function works", {
   expect_equal(plus_five(6), 11)
+  expect_equal(plus_six(6), 12)
+  expect_equal(length(testtab()), 1000)
 })
